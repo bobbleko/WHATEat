91c9df3011b7e0e8223f240cabe10f650cedebda
anunpong <bobblebleble@gmail.com>
2018-12-26 15:58:36 +0700

    Finish 26 DEC

--- app/src/main/java/anunpongi/th/ac/rmutl/whateat/FormFragment.java
+++ app/src/main/java/anunpongi/th/ac/rmutl/whateat/FormFragment.java
@@ -2,10 +2,18 @@ package anunpongi.th.ac.rmutl.whateat;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
 
 
 /**
@@ -13,9 +21,92 @@ import android.view.ViewGroup;
  */
 public class FormFragment extends Fragment {
 
+    private String nameString, surnameString, genderString,
+            ageString, statureString;
+
 
     public FormFragment() {
         // Required empty public constructor
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+
+//        Gender Controller
+        genderController();
+
+//        Save Controller
+        saveController();
+
+//        Age Controller
+        ageController();
+
+
+
+    }   //Main Methon
+
+    private void ageController() {
+        String[] strings = new String[]{"0","1","2","3","4","5","6",};
+        Spinner spinner = getView().findViewById(R.id.spnAge);
+
+
+    }
+
+    private void genderController() {
+        RadioGroup radioGroup = getView().findViewById(R.id.ragGender);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+
+
+                switch (checkedId) {
+                    case R.id.radMale:
+                        genderString = "0";
+                        break;
+                    case R.id.radFamale:
+                        genderString = "1";
+                        break;
+
+                }
+
+            }
+        });
+    }
+
+    private void saveController() {
+        Button button = getView().findViewById(R.id.btnSave);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                EditText nameEditText = getView().findViewById(R.id.edtName);
+                EditText surnameEditText = getView().findViewById(R.id.edtSurname);
+
+                nameString = nameEditText.getText().toString().trim();
+                surnameString = surnameEditText.getText().toString().trim();
+
+                MyAlert myAlert = new MyAlert(getActivity());
+
+                RadioButton maleRadioButton = getView().findViewById(R.id.radMale);
+                RadioButton famaleRedioButton = getView().findViewById(R.id.radFamale);
+
+//                Check Space
+                if (nameString.isEmpty() || surnameString.isEmpty() )
+                         {
+//                    Have Space
+                    myAlert.normalDialog("Have Space", "Please Fill Every Blank");
+
+                } else if (!(maleRadioButton.isChecked() || famaleRedioButton.isChecked())) {
+//                    Non Choose Gerder
+                    myAlert.normalDialog("Non Gender","Please Choose Male or Famele");
+                }
+
+
+            } //onClick
+        });
     }
 
 

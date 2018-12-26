3d23078d338962bc39fb88a0ee0e8a9aece88b47
anunpong <bobblebleble@gmail.com>
2018-12-26 11:50:37 +0700

    Toolber Success

--- app/src/main/java/anunpongi/th/ac/rmutl/whateat/MainActivity.java
+++ app/src/main/java/anunpongi/th/ac/rmutl/whateat/MainActivity.java
@@ -2,6 +2,7 @@ package anunpongi.th.ac.rmutl.whateat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Create Toolber
+        createToolber();
+
+
+    } //Main Method
+
+    private void createToolber() {
+        Toolbar toolbar = findViewById(R.id.toolbarMain);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(getString(R.string.title_toolber));
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.ic_action_hambager);
     }
-}
+
+}   //Main Class
